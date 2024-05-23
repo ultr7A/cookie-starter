b0b50c985c2b4830fe5327d622a0bcba278d029b
J. R. Evans <ultr7a@gmail.com>
2024-05-22 23:09:59 -0400

    add debugging feature for development

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-
-# clone the repository
 git clone https://github.com/ultr7A/cookie-star.git
+
+// check if COOKIE_STAR_BRANCH is set and checkout if so
+if [ -n "$COOKIE_STAR_BRANCH" ]; then
+  cd cookie-star
+  git checkout $COOKIE_STAR_BRANCH
+  cd ..
+fi
 
 ./update.sh
